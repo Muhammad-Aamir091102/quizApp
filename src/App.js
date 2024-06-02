b0a11ec298503e9c623476ea5a6efd3a83287b42
hammadmughal12072004 <hammadanwaar04@gmail.com>
2024-06-02 20:29:12 +0500

    Resolved conflicts during rebase

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Components/style.css'; 
+import SignInSignUp from './Components/signinout';
+
+function App() {
+  return (
+    <div className="App">
+      <SignInSignUp />
+    </div>
+  );
+}
+
+export default App;
